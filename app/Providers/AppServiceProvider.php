7f102552d4acedf4311c127ca7b42cd7a2d627c3
ShunyaSuzuki <anyushu2017@gmail.com>
2020-10-28 14:57:52 +0900

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,30 +26,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] != null) {
-            $url = $_SERVER['HTTP_HOST'];
-            $domain_array = explode('.', $url);
-            $sub_domain = $domain_array[0];
-            $manages = DB::table('manages')->where('domain', $sub_domain)->first();
-
-            if ($manages !== null) {
-                if (session('receipt.shop_id') != null) {
-                    $manages_shops = DB::table('shops')->find(session('receipt.shop_id'));
-                } else {
-                    $manages_shops = DB::table('shops')->where('manages_id', $manages->id)->first();
-                }
-                $title = $manages->name;
-                $description = $manages->description;
-                View::share([
-                    'sub_domain' => $sub_domain,
-                    'meta_title' => $title,
-                    'meta_description' => $description,
-                    'manages' => $manages,
-                    'manages_shops' => $manages_shops,
-                ]);
-            } else {
-                abort('404');
-            }
+        $manages = DB::table('manages')->where('domain', 'mk-goto')->first();
+        if (session('receipt.shop_id') != null) {
+            $manages_shops = DB::table('shops')->find(session('receipt.shop_id'));
+        } else {
+            $manages_shops = DB::table('shops')->where('manages_id', $manages->id)->first();
         }
+        $title = $manages->name;
+        $description = $manages->description;
+        View::share([
+            'sub_domain' => 'mk-goto',
+            'meta_title' => $title,
+            'meta_description' => $description,
+            'manages' => $manages,
+            'manages_shops' => $manages_shops,
+        ]);
     }
 }
